782348f2016e579ba84112e34ac2c67156e65634
gpsoft <etoh.gpsoft@gmail.com>
2017-09-17 22:53:11 +0900

    Add wrap, pad, datetime

--- public/app.php
+++ public/app.php
@@ -1,5 +1,9 @@
 <?php
 
+function esc($s) {
+    return htmlspecialchars($s, ENT_QUOTES);
+}
+
 function h($caption) {
     echo '<h2>'.$caption.'</h2>';
 }
@@ -15,31 +19,33 @@ function to_s($d) {
 }
 
 function eval_print($code, $ret=null) {
-    echo '<tr><td><code>'.$code.'</code></td>';
+    echo '<tr><td><code>'.$code.';</code></td>';
     if ( is_null($ret) ) {
         eval('$ret='.$code.';');
     }
-    echo '<td><code>'.to_s($ret).'</code></td></tr>';
+    echo '<td><code>'.esc(to_s($ret)).'</code></td></tr>';
 }
 
 function in_table($f) {
     echo '<table class="table table-bordered">';
-    echo '<tr><th>Code</th><th>Result</th></tr>';
+    echo '<tr><th class="col-xs-6">Code</th><th class="col-xs-6">Result</th></tr>';
     $f();
     echo '</table>';
 }
 
 function run() {
-    h('親切な、空っぽチェック');
+    h('空っぽチェック');
     in_table(function(){
         eval_print('u_empty(null)');
         eval_print('u_empty("")');
         eval_print('u_empty("0")');
         eval_print('u_empty(false)');
         eval_print('u_empty(array())');
+        eval_print('u_coalesce(array(1,2,3), "no data")');
+        eval_print('u_coalesce(array(), "no data")');
     });
 
-    h('安全な、配列アクセス');
+    h('配列アクセス');
     in_table(function(){
         $data =<<<EOS
 [
@@ -53,6 +59,8 @@ function run() {
 EOS;
         code('$data = '.$data);
         eval('$data = '.$data);
+        eval_print('u_at(["apple", "cake", "rocket"], 0)');
+        eval_print('u_at(["apple", "cake", "rocket"], -1)');
         eval_print('u_at($data, "fuga")', u_at($data, "fuga"));
         eval_print('u_at(null, "hoge")');
         eval_print('u_at(123, "hoge")');
@@ -64,5 +72,104 @@ EOS;
         eval_print(
             'u_at($data, array("piyo", "bar", 2))',
             u_at($data, array("piyo", "bar", 2)));
+        eval_print(
+            'u_at($data, array("piyo", "bar", -1))',
+            u_at($data, array("piyo", "bar", -1)));
+    });
+    in_table(function(){
+        $data =<<<EOS
+[
+    'string'=>'abc',
+    'integer'=>25,
+    'array'=>['A', 'B', 'C'],
+    'bool'=>true
+];
+EOS;
+        code('$data = '.$data);
+        eval('$data = '.$data);
+        eval_print(
+            'u_ats($data, "string")',
+            u_ats($data, "string"));
+        eval_print(
+            'u_ats($data, "string2")',
+            u_ats($data, "string2"));
+        eval_print(
+            'u_ati($data, "integer")',
+            u_ati($data, "integer"));
+        eval_print(
+            'u_ati($data, "integer2")',
+            u_ati($data, "integer2"));
+        eval_print(
+            'u_ata($data, "array")',
+            u_ata($data, "array"));
+        eval_print(
+            'u_ata($data, "array2")',
+            u_ata($data, "array2"));
+        eval_print(
+            'u_atb($data, "bool")',
+            u_atb($data, "bool"));
+        eval_print(
+            'u_atb($data, "bool2")',
+            u_atb($data, "bool2"));
+    });
+
+    h('囲む');
+    in_table(function(){
+        eval_print('u_wrap("question?", "(")');
+        eval_print('u_wrap("question?", "[")');
+        eval_print('u_wrap("question?", "<")');
+        eval_print('u_wrap("question?", "/")');
+        eval_print('u_wrapsq("SELECT * FROM hoge")');
+        eval_print('u_wrapdq("SELECT * FROM hoge")');
+        eval_print('u_wraptag("Hello, world!", "p")');
+        eval_print("u_wraptag('Hello, world!', 'p class=\"danger\"')");
+    });
+
+    h('区切り文字列');
+    in_table(function(){
+        eval_print("u_encA2S([1,2,3])");
+        eval_print("u_encA2S([1,2,3], '|')");
+        eval_print("u_decS2A('1,2,3')");
+        eval_print("u_decS2A('')");
+        eval_print("u_decS2A('huh?')");
+        eval_print("u_decS2A(',huh?,')");
+    });
+
+    h('日時(正規形)');
+    in_table(function(){
+        eval_print("u_date('hello')");
+        eval_print("u_date('1991')");
+        eval_print("u_date('20170908')");
+        eval_print("u_date('20170908123456')");
+        eval_print("u_date('2017/09/08')");
+        eval_print("u_datey('2017/09/08')");
+        eval_print("u_datem('20170908')");
+        eval_print("u_dated('20170908')");
+        eval_print("u_dated('201709')");
+    });
+    in_table(function(){
+        eval_print("u_time4('hello')");
+        eval_print("u_time4('1234')");
+        eval_print("u_time4('12:34')");
+        eval_print("u_time4('20170908123456')");
+        eval_print("u_time6('1234')");
+        eval_print("u_time6('12:34')");
+        eval_print("u_time6('12:34:56')");
+        eval_print("u_time6('20170908123456')");
+        eval_print("u_timeh('12:34:56')");
+        eval_print("u_timem('12:34:56')");
+        eval_print("u_times('12:34:56')");
+    });
+    in_table(function(){
+        eval_print("u_datetime('hello')");
+        eval_print("u_datetime('2017/09/08 12:34:56')");
+        eval_print("u_datetime('20170908123456')");
+    });
+
+    h('パディング');
+    in_table(function(){
+        eval_print("u_padsp('hello', 10)");
+        eval_print("u_pad('hello', -10, '_')");
+        eval_print("u_pad0('64', 4)");
     });
 }
--- public/util.php
+++ public/util.php
@@ -6,6 +6,11 @@ function u_empty($d) {
         (!is_string($d) && empty($d));
 }
 
+function u_coalesce($a, $b) {
+    if ( u_empty($a) ) return $b;
+    return $a;
+}
+
 function u_at($a, $ks, $def=null) {
     if ( is_null($a) ) return $def;
     if ( !is_array($a) ) return $a;
@@ -17,7 +22,72 @@ function u_at($a, $ks, $def=null) {
     }
 
     $k = $ks;
+    if ( is_int($k) && $k < 0 && count($a) > -$k ) $k += count($a);
     if ( !array_key_exists($k, $a) ) return $def;
     return $a[$k];
 }
+function u_ata($a, $ks) { return u_at($a, $ks, array()); }
+function u_ats($a, $ks) { return u_at($a, $ks, ''); }
+function u_ati($a, $ks) { return (int)u_at($a, $ks, 0); }
+function u_atb($a, $ks) { return (boolean)u_at($a, $ks, false); }
 
+function u_wrap($s, $w) {
+    $ps = array(
+        '('=>')',
+        '['=>']',
+        '<'=>'>',
+    );
+    if ( is_null($s) ) $s = '';
+    $wb = $w;
+    $we = u_at($ps, $w, $w);
+    return $wb.$s.$we;
+}
+function u_wrapsq($s) { return u_wrap($s, "'"); }
+function u_wrapdq($s) { return u_wrap($s, '"'); }
+function u_wraptag($s, $t) {
+    return '<'.$t.'>'.$s.'</'.u_at(explode(' ', $t), 0).'>';
+}
+
+function u_decS2A($s, $sepa=',') {
+    return u_empty($s) ? array() : explode($sepa, $s);
+}
+function u_encA2S($a, $sepa=',') {
+    return implode($sepa, $a);
+}
+
+function u_pad($s, $len, $pad) {
+    if ( is_null($s) ) $s = '';
+    $l = abs($len);
+    $fill = str_repeat($pad, $l);
+    $l = max(mb_strlen($s), $l);
+    if ( $len > 0 ) return mb_substr($fill.$s, -$l);
+    return mb_substr($s.$fill, 0, $l);
+}
+function u_padsp($s, $len) { return u_pad($s, $len, ' '); }
+function u_pad0($s, $len) { return u_pad($s, $len, '0'); }
+
+function u_date($s) {
+    $d = preg_replace('/[^0-9]/', '', $s);
+    if ( strlen($d) < 8 ) return '';
+    return substr($d, 0, 8);
+}
+function u_time($s, $sec=true) {
+    $t = preg_replace('/[^0-9]/', '', $s);
+    if ( strlen($t) < 4 ) return '';
+    if ( strlen($t) >= 12 ) $t = substr($t, 8);
+    if ( $sec && strlen($t) == 4 ) $t = $t.'00';
+    return substr($t, 0, $sec?6:4);
+}
+function u_datetime($s) {
+    $d = preg_replace('/[^0-9]/', '', $s);
+    if ( strlen($d) < 14 ) return '';
+    return substr($d, 0, 14);
+}
+function u_datey($s) { return u_coalesce(substr(u_date($s), 0, 4), ''); }
+function u_datem($s) { return u_coalesce(substr(u_date($s), 4, 2), ''); }
+function u_dated($s) { return u_coalesce(substr(u_date($s), 6, 2), ''); }
+function u_time6($s) { return u_time($s, true); }
+function u_time4($s) { return u_time($s, false); }
+function u_timeh($s) { return u_coalesce(substr(u_time($s), 0, 2), ''); }
+function u_timem($s) { return u_coalesce(substr(u_time($s), 2, 2), ''); }
+function u_times($s) { return u_coalesce(substr(u_time($s), 4, 2), ''); }
